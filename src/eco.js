00e90df23e711c377f2b32f930ef37b33021f1e7
Caleb Ji <30414755+calebji123@users.noreply.github.com>
2022-11-29 10:15:17 -0500

    Update from view

--- src/eco.js
+++ src/eco.js
@@ -1,11 +1,33 @@
-const resources = document.getElementById('money')
+const resources = document.getElementById('resource')
 
 let eco = {
     res: {
         'wood':0,
         'stone':0,
         'energy':0,
-        'food':0
+        "grain":0,
+        'food':0,
+        "brick":0,
+        'green octagon':0,
+        "bomb":0,
+        "livestock":0,
+        "cloth":0,
+        "oil":0,
+        "orange diamond":0,
+    },
+    icon: {
+        'wood': "🪵",
+        'stone':"🪨",
+        'energy': "⚡",
+        "grain": "🌾",
+        'food': "🍖",
+        'brick': "🧱",
+        'green octagon': "💎",
+        "bomb":"💣",
+        "livestock": "🐮",
+        "cloth":"🧶",
+        "oil":"🛢",
+        "orange diamond":"🔶",
     },
     set add(obj) {
         for (const k in obj) {
@@ -26,12 +48,35 @@ let eco = {
         }
         return true
     },
+    parse_num(num){
+        const thousand = 1000
+        const million = 1000000
+        const billion = 1000000000
+        const trillion = 1000000000000
+        const quadrillion = 1000000000000000
+        if(num < 10*thousand){
+            return num
+        }
+        else if (num < million){
+            return ((num / thousand).toString().slice(0,4).replace(/^\.+|\.+$/g, '') + "k")
+        }
+        else if (num < billion){
+            return ((num / million).toString().slice(0,4).replace(/^\.+|\.+$/g, '') + "M")
+        }
+        else if (num < trillion){
+            return ((num / billion).toString().slice(0,4).replace(/^\.+|\.+$/g, '') + "B")
+        }
+        else if (num < quadrillion){
+            return ((num / trillion).toString().slice(0,4).replace(/^\.+|\.+$/g, '') + "T")
+        }
+        return "big num"
+    },
     updateRes() {
         resources.innerHTML = ''
         for (const k in this.res) {
             let s = document.createElement('p')
-            s.classList += "resource"
-            s.innerHTML = `${k}: ${this.res[k]} `
+            s.classList += "resource-item"
+            s.innerHTML = `${this.parse_num(this.res[k])} <br> ${this.icon[k]}`
             resources.appendChild(s)
         }
     }
